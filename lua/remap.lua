5983f13601da6e403b2399df48ec8a12c54c9465
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-05-07 13:29:40 -0400

    Added <leader> shortcuts for bp and bn. Might change these later

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
+vim.keymap.set("n", "<leader>bn", vim.cmd.bn)
 
 vim.keymap.set('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<cr>')
 vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
