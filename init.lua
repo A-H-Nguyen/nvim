697110de45987b404175c7c251e6e778f8fd0c35
andrew <a.h.nguyen475@gmail.com>
2024-05-07 20:54:54 -0400

    Added background transparency! TODO: Add the commenter plugin

--- init.lua
+++ init.lua
@@ -5,3 +5,6 @@ require("LSP_actions")
 
 vim.o.termguicolors = true
 vim.cmd.colorscheme "catppuccin"
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
