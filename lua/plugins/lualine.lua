4573fd08a1abd949eafb1393bd4439a2a983eb1e
andrew <a.h.nguyen475@gmail.com>
2024-05-21 14:27:11 -0400

    Changed theme -- I had a problem where I can't change the sections from default

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,15 @@
 return {
     "nvim-lualine/lualine.nvim",
 
-    dependencies = { 
-        "nvim-tree/nvim-web-devicons" 
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
     },
 
     config = function()
         require('lualine').setup({
             options = {
                 icons_enabled = true,
-                theme = "ayu_dark"
-                -- section_separators = { left = '', right = '' },
-                -- component_separators = { left = '', right = '' }
+                theme = 'auto',
             }
         })
     end
