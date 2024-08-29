63d95e8e0fe8a48ac4000b7f5cb485d571ccb6e8
Andrew Nguyen <andrewnguyen@utexas.edu>
2024-08-29 17:40:23 -0500

    I like the context plugin

--- lua/plugins/treesitter_context.lua
+++ lua/plugins/treesitter_context.lua
@@ -2,9 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter-context",
 
 	config = function ()
-		require("treesitter-context").setup({
-			separator = '-'
-		})
+		require("treesitter-context").setup({})
     -- hi TreesitterContextBottom gui=underline guisp=Grey
     -- hi TreesitterContextLineNumberBottom gui=underline guisp=Grey
 	end
