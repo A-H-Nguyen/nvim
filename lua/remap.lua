219ee900d13f76b0d1ac1c7306c9f1deaf2ce12d
Andrew Nguyen <andrewnguyen@utexas.edu>
2025-05-30 18:02:28 -0500

    Make next and prev buffer more similar to standard vim cmd

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,10 +2,8 @@ vim.g.mapleader = " "
 
 -- Spruce up file navigation
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
--- vim.keymap.set("n", "<leader>bn", vim.cmd.bn)
-vim.keymap.set("n", "<leader>p", vim.cmd.bp)
-vim.keymap.set("n", "<leader>n", vim.cmd.bn)
+vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
+vim.keymap.set("n", "<leader>bn", vim.cmd.bn)
 
 -- Shortcuts for splitting windows
 vim.keymap.set("n", "<leader>_", vim.cmd.sp)
