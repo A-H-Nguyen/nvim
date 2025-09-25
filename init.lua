7546396cca3ed5d299d5741a4cabc206e4a35536
Andrew Nguyen <andrewnguyen@utexas.edu>
2025-09-25 10:26:25 -0500

    Fixed the colored column persisting after I change buffer from a C/C++ file

--- init.lua
+++ init.lua
@@ -7,24 +7,25 @@ vim.api.nvim_set_option("clipboard", "unnamedplus")
 vim.o.termguicolors = true
 vim.cmd.colorscheme "catppuccin"
 
-vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalSB",    { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 -- vim.g.clipboard = {
 --         name = 'myClipboard',
 --         copy = {
---            ['+'] = 'xfce4-clipman',
---            ['*'] = 'xfce4-clipman',
---          },
+--            ['+'] = 'waycopy',
+--            ['*'] = 'waycopy',
+--         },
 --         paste = {
---            ['+'] = 'xfce4-clipman',
---            ['*'] = 'xfce4-clipman',
+--            ['+'] = 'waycopy',
+--            ['*'] = 'waycopy',
 --         },
 --         cache_enabled = 1,
 --       }
 
+-- For if I have stuff in a .vimrc such as code coloring for MLIR
 vim.cmd([[
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
