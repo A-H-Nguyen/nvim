f804d78ab419cc159b4d0c0661c29b44c31af60f
Andrew Nguyen <andrewnguyen@utexas.edu>
2026-07-22 13:24:09 -0500

    Simplified my lualine config. I don't need most of the info from the default settings. Keep them as a comment in case I change my mind.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,21 +11,29 @@ return {
           icons_enabled = true,
           theme = 'auto',
         },
+        -- sections = {
+        --   lualine_a = {'mode'},
+        --   lualine_b = {'branch', 'diff', 'diagnostics'},
+        --   lualine_c = {
+        --     {'filename', path=1,}
+        --   },
+        --   lualine_x = {'encoding', 'fileformat', 'filetype'},
+        --   lualine_y = {'progress'},
+        --   lualine_z = {'location'}
+        -- },
         sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {
-            {'filename', path=1,}
-          },
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
+          lualine_a = {},
+          lualine_b = {{'filename', path=1,}},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
           lualine_z = {'location'}
         },
         inactive_sections = {
           lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
+          lualine_b = {'filename'},
+          lualine_c = {},
+          lualine_x = {},
           lualine_y = {},
           lualine_z = {}
         },
