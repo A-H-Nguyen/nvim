96d9baba59632c2383a454daac581338329be0f0
Andrew Nguyen <andrewnguyen@utexas.edu>
2026-09-15 16:05:21 -0500

    Made my lualine more sparse -- didn't need all the info that was there previously

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -26,12 +26,12 @@ return {
           lualine_b = {{'filename', path=1,}},
           lualine_c = {},
           lualine_x = {},
-          lualine_y = {},
-          lualine_z = {'location'}
+          lualine_y = {'location'},
+          lualine_z = {}
         },
         inactive_sections = {
           lualine_a = {},
-          lualine_b = {'filename'},
+          lualine_b = {},
           lualine_c = {},
           lualine_x = {},
           lualine_y = {},
